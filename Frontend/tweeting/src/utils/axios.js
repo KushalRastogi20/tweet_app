c131ecbb972d4a293812252902192b52fe8621ed
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2025-09-20 01:24:21 +0530

    Axios updated

--- Frontend/tweeting/src/utils/axios.js
+++ Frontend/tweeting/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // points to Next.js API
+  // baseURL: "/api", // points to Next.js API
+  baseURL: "https://tweet-app-beige.vercel.app/api",
   withCredentials: true, // if you use cookies
 });
 
